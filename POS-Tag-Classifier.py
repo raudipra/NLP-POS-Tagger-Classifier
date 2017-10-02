169ac84fe005324bd482e2d39741ced5b9ccadd1
raudipra <raudiradz@gmail.com>
2017-10-02 11:32:25 +0700

    cleaning code, add multiple classifier

--- POS-Tag-Classifier.py
+++ POS-Tag-Classifier.py
@@ -9,6 +9,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.feature_extraction import DictVectorizer
 import numpy
 
 X = []
@@ -36,73 +39,36 @@ for line in f:
       'is_numeric': sentence[1].isdigit(),
       'capitals_inside': sentence[1][1:].lower() != sentence[1][1:] }
     )
-    # X.append( [
-    #   sentence[1],
-    #   sentence[0] == '1',
-    #   sentence[0] == len(sentence) - 1,
-    #   sentence[1][0].upper() == sentence[1][0],
-    #   sentence[1].upper() == sentence[1],
-    #   sentence[1].lower() == sentence[1],
-    #   sentence[1][0],
-    #   sentence[1][:2],
-    #   sentence[1][:3],
-    #   sentence[1][-1],
-    #   sentence[1][-2:],
-    #   sentence[1][-3:],
-    #   '' if sentence[0] == '1' else y[len(X)-1],
-    #   '' if sentence[0] == '1' else X[len(X)-1][0],
-    #   '-' in sentence[1],
-    #   sentence[1].isdigit(),
-    #   sentence[1][1:].lower() != sentence[1][1:] ]
-    # )
     y.append(sentence[4])
-#    print len(X)
-#    print len(y)
-print len(X)
-print len(y)
+    
+print "Feature data size : "+len(X)
+print "Label data size : "+len(y)
 
-from sklearn.feature_extraction import DictVectorizer
 v = DictVectorizer(sparse=False)
 X = v.fit_transform(X)
 
+print "Dividing dataset into training set and testing set ..."
 cutoff = int(.75 * len(X))
 training_sentences = X[:cutoff]
 training_tags = y[:cutoff]
 test_sentences = X[cutoff:]
 test_tags = y[cutoff:]
- 
-print len(training_sentences)   
-print len(test_sentences)
+
+print "Training set size : "+len(training_sentences)   
+print "Testing set size : "+len(test_sentences)
 
 
-# clf = Pipeline([
-#     ('vectorizer', DictVectorizer(sparse=False)),
-#     # ('classifier', DecisionTreeClassifier(criterion='entropy'))
-#     ('classifier', SGDClassifier(loss='log'))
-# #    ('classifier', svm.SVC(gamma=0.001, C=100.))
-# ])
-# v = DictVectorizer(sparse=False)
-# print training_sentences_1
-# training_sentences = v.fit_transform(training_sentences_1)
-# training_tags = v.fit_transform(training_tags_1)
+#clf = SGDClassifier(loss='log')
+#clf = svm.SVC(decision_function_shape='ovo')
+clf = MLPClassifier(solver='adam', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
 
-clf = SGDClassifier(loss='log')
-
-n_iter = 15
+print 'Training started'
+n_iter = 10
 for n in range(n_iter):
     clf.partial_fit(training_sentences[(n*10000):((n+1)*10000)], training_tags[(n*10000):((n+1)*10000)],classes=numpy.unique(training_tags))  
-
+    print "Training progress "+(n*10000)
 print 'Training completed'
  
-# X_test, y_test = transform_to_dataset(test_sentences)
- 
-print "Accuracy:", clf.score(test_sentences, test_tags)
-
-
-# print X
-
-
-# print v.inverse_transform(X) ==         [{'bar': 2.0, 'foo': 1.0}, {'baz': 1.0, 'foo': 3.0}]
-
-# print v.transform({'foo': 4, 'unseen_feature': 3})
-
+print "Testing started"
+score = clf.score(test_sentences, test_tags)
+print "Accuracy:", score
