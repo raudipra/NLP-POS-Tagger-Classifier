e3b20bb06092c2fe835cfa9e9ff66ae1f38850bd
rmxhaha <code000123@gmail.com>
2017-10-02 20:53:42 +0700

    adding configuration

--- POS-Tag-SVM.py
+++ POS-Tag-SVM.py
@@ -13,6 +13,42 @@ from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction import DictVectorizer
 import numpy
+import json
+import sys
+
+default_configuration_raw = """
+{
+  "features": [ "word",
+      "is_first",
+      "is_last",
+      "is_capitalized",
+      "is_all_caps",
+      "is_all_lower",
+      "prefix-1",
+      "prefix-2",
+      "prefix-3",
+      "suffix-1",
+      "suffix-2",
+      "suffix-3",
+      "prev_tag",
+      "prev_word",
+      "has_hyphen",
+      "is_numeric",
+      "capitals_inside" 
+  ],
+  "algorithm": "MLP"
+}
+
+"""
+
+configuration = json.loads(default_configuration_raw)
+
+if( len(sys.argv) >= 2 ):
+	config_file = sys.argv[1]
+	with open(config_file,'r') as f:
+		configuration = json.loads(f.read())
+
+
 
 X = []
 y = []
@@ -20,7 +56,7 @@ f = open('id-ud-train+dev.conllu', 'r')
 for line in f:
   sentence = line.split("\t")
   if sentence[0].isdigit():
-    X.append( {
+  	data = {
       'word': sentence[1],
       'is_first': sentence[0] == '1',
       'is_last': sentence[0] == len(sentence) - 1,
@@ -37,8 +73,10 @@ for line in f:
       'prev_word': '' if sentence[0] == '1' else X[len(X)-1]['word'],
       'has_hyphen': '-' in sentence[1],
       'is_numeric': sentence[1].isdigit(),
-      'capitals_inside': sentence[1][1:].lower() != sentence[1][1:] }
-    )
+      'capitals_inside': sentence[1][1:].lower() != sentence[1][1:] 
+  	}
+
+    X.append({ k: v for k,v in data.items() if k in configuration.features })
     y.append(sentence[3])
     
 print "Feature data size : "+str(len(X))
@@ -58,11 +96,16 @@ test_tags = y[cutoff:]
 print "Training set size : "+str(training_sentences.shape[0])  
 print "Testing set size : "+str(test_sentences.shape[0])
 
+# default SGD
+clf = SGDClassifier(loss='log')
 
-epoch = 200
-#clf = SGDClassifier(loss='log')
-#clf = svm.SVC(decision_function_shape='ovo')
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(17, 17,17,17), random_state=1,max_iter=epoch)
+if( configuration.algorithm == "SGD")
+	clf = SGDClassifier(loss='log')
+elif( configuration.algorithm == "SVM")
+	clf = svm.SVC(decision_function_shape='ovo')
+elif ( configuration.algorithm == "MLP" )
+	epoch = 200
+	clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(17, 17,17,17), random_state=1,max_iter=epoch)
 
 print 'Training started'
 clf.fit(training_sentences, training_tags)  
